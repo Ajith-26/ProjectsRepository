a825cfe411097810fcb2d26d52e17d97b8b0cb4e
Ajith-26 <ajithkumarjaisankar@gmail.com>
2022-04-26 12:35:09 +0530

    Threat detection application

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/27/907e671f8ac4001c1b6fff38445ca2c6
@@ -0,0 +1,12 @@
+package com.zoho.client;
+
+import com.zoho.fetchlogs.AnalyseFirewallLogs;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		AnalyseFirewallLogs.analyseLogs();
+
+	}
+
+}
--- ThreatDetectionApplication/src/com/zoho/client/MainApp.java
+++ ThreatDetectionApplication/src/com/zoho/client/MainApp.java
@@ -1,12 +1,9 @@
 package com.zoho.client;
-
-import com.zoho.fetchlogs.AnalyseFirewallLogs;
+import com.zoho.fetchlogs.LogsAnalysis;
 
 public class MainApp {
-
-	public static void main(String[] args) {
-		AnalyseFirewallLogs.analyseLogs();
-
+	public static void main(String[] args) throws Exception{
+		LogsAnalysis  service = new LogsAnalysis();
+		service.analyseLogs();
 	}
-
 }
